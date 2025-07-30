8584c88dbcd9d16248becf9cdde0ce9663e72189
Fedora <Fedora>
2025-07-30 10:10:01 -0300

    Base pequena de algoritmos e estrutura de dados

---
+++ py/estrutura_de_dados.py
@@ -0,0 +1,23 @@
+# Lista
+
+lista = ["maça", "banana", "uva"]
+print(lista[0])
+
+# Dicionário
+
+dicionario = {"nome": "jose", "idade": 25 }
+print(dicionario["nome"])
+
+# Conjunto
+
+conjunto = {1, 2, 2, 3, 4}
+print(conjunto)
+
+# Tupla
+
+tupla = (1, 2, 3)
+print(tupla[0])
+
+# Fila
+
+# Pilha
